31e3617221da374c754f6bdca4ef80e254495460
Kevin Wang <wangkevin448@gmail.com>
2018-03-11 18:16:19 -0700

    Add predefined constants for iteration

--- Makefile
+++ Makefile
@@ -15,6 +15,7 @@ $(BIN): $(BIN_SRC)
 
 test: $(BIN)
 	./$(BIN) > $(TEST_SRC)
+	cat $(TEST_SRC)
 	-diff -u $(BIN_SRC) $(TEST_SRC)
 
 clean:
--- quine.c
+++ quine.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
+#define ITER 0
+#define END 4
+
 int main() {
-  char* s = "#include <stdio.h>%c%cint main() {%c  char* s = %c%s%c;%c  printf(s,10,10,10,34,s,34,10,10,10,10);%c  return 0;%c}%c";
-  printf(s,10,10,10,34,s,34,10,10,10,10);
+  char* s = "#include <stdio.h>%c%c#define ITER %d%c#define END %d%c%cint main() {%c  char* s = %c%s%c;%c  printf(s,10,10,ITER,10,END,10,10,10,34,s,34,10,10,10,10);%c  return 0;%c}%c";
+  printf(s,10,10,ITER,10,END,10,10,10,34,s,34,10,10,10,10);
   return 0;
 }
